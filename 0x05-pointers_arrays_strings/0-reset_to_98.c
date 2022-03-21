23753ecedbc347dd336a6537db4ae2806f6d938a
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-03-22 00:29:15 +0100

    changing what the pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void){
+void reset_to_98(int *n)
+	*n = 98;
+
+}
+
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int_strlen(char *s);
+
+
+
+
+#endif
