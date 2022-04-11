a8b0945a59eb8532d412b798d7cb9420b187f119
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-04-11 18:51:15 +0100

    preprocessor task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef PREPROCESSOR_H
+#define PREPROCESSOR_H
+
+#define SIZE 1024
+typedef int make_iso_compilers_happy;
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+ Learning about preprocessors in in C is somewhat interesting to me.
